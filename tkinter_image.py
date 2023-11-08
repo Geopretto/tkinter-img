eeadb9f0df76b0ab9db36c5507f8959c7c2fb087
Geovana <prettogeovana11@gmail.com>
2023-11-08 20:55:03 -0300

    primeira imagem

---
+++ tkinter_image.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+
+
+img = Image.open('gato.jpg')
+img_tk = ImageTk.PhotoImage(img)
+
+
+img_label = Label(root, image=img_tk)
+
+img_label.pack()
+
+root.mainloop()
